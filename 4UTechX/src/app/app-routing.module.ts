bb0e72738f6be5d1cb6f2045026528cc6f97e75c
4utechx <141213926+4utechx@users.noreply.github.com>
2023-08-24 11:22:52 +0530

    careers page created

--- 4UTechX/src/app/app-routing.module.ts
+++ 4UTechX/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomePageComponent} from '../app/home-page/home-page.component'
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { CareersComponent } from './careers/careers.component';
 
 const routes: Routes = [
   {path:"", redirectTo:"home", pathMatch:"full"},
   {path:"home", component:HomePageComponent},
   {path:"contactus", component:ContactUsComponent},
-  {path:"aboutus", component:AboutUsComponent}
+  {path:"aboutus", component:AboutUsComponent},
+  {path:"carrers", component:CareersComponent}
 ];
 
 @NgModule({
